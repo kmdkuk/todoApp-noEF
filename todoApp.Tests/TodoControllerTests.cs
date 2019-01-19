fe5d9cb4de2361ea52fd3f9aaaa4da79aade7e6a
kouki <kouworld0123@gmail.com>
2019-01-19 11:58:56 +0900

    :+1: テストの追加

---
+++ todoApp.Tests/TodoControllerTests.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using todoApp.Controllers;
+using todoApp.Models.Persistance;
+using Xunit;
+using Moq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TodoApi.Models;
+using todoApp.Models.Repository;
+using System;
+
+namespace todoApp.Tests
+{
+    public class TodoControllerTests
+    {
+        [Fact]
+        public async Task Test1Async()
+        {
+            var itemlist = new List<TodoItem>()
+            {
+                new TodoItem{ Id = Guid.NewGuid().ToString("N"), Name = "Test1", IsComplete = false},
+                new TodoItem{ Id = Guid.NewGuid().ToString("N"), Name = "Test2", IsComplete = false},
+                new TodoItem{ Id = Guid.NewGuid().ToString("N"), Name = "Test3", IsComplete = false}
+            };
+            var uowMoq = new Mock<IUnitOfWork>();
+            var todoItemsRepoMoq = new Mock<ITodoItemRepository>();
+            todoItemsRepoMoq.Setup(x => x.FindAllAsync()).ReturnsAsync(itemlist);
+            uowMoq.Setup(x => x.TodoItems).Returns(todoItemsRepoMoq.Object);
+
+            var controller = new TodoController(uowMoq.Object);
+
+
+            var result = await controller.GetTodoItems();
+
+            Assert.IsType<ActionResult<IEnumerable<TodoItem>>>(result);
+        }
+    }
+}
